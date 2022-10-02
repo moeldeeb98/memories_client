79f5d0ac3d90779dac2d75c1c7f8a8d470f596ec
Mohamed A. Eldeeb <meldeeb1998@gmail.com>
2022-10-02 17:25:39 +0200

    init the app page

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { createUseStyles } from "react-jss";
+export default createUseStyles({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "rgba(0,183,255, 1)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+});
